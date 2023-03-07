461efbf6357e340442e2a047123bddb790e1e5cc
lihaan <45350323+lihaan@users.noreply.github.com>
2023-03-08 01:39:56 +0800

    finish home page and scaffold convert function

--- smolify-web/src/App.js
+++ smolify-web/src/App.js
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import convert from "./convert";
+
+
 
 function App() {
+
+  const hostname = "smoli.fy"
+  const [shortenedLink, setShortenedLink] = useState("")
+  const [longLink, setLongLink] = useState("")
+
+  function handleClick() {
+    let newLink = convert(longLink)
+    setShortenedLink(newLink)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Smolify</h1>
-        <input type="text" id="inputLink" size="22" placeholder='Insert not smol link here'/>
+        <h1 id="h1Title">Smolify</h1>
+        <p id="pCaption">Give me a URL and I'll make it smol and friendly!</p>
+        <input
+          type="text"
+          id="inputLink"
+          size="22"
+          placeholder="Insert not smol URL here"
+          onChange={event => setLongLink(event.target.value)}
+          value={longLink}
+        />
+        <button type="button" id="buttonSubmit" onClick={handleClick}>
+          Shorten
+        </button>
+        <div id="divShortenedContainer">
+          <p id="pShortenedHeader">Your shortened link is:</p>
+          <a id="aShortenedLink" href={shortenedLink}>{hostname}/{shortenedLink}</a>
+        </div>
       </header>
     </div>
   );
---
+++ smolify-web/src/convert.js
@@ -0,0 +1,12 @@
+function convert(longLink) {
+    console.log(longLink)
+    
+    // make api call
+        // remove http and https
+        // http-1, https-2
+
+
+    return "api-result"
+}
+
+export default convert
