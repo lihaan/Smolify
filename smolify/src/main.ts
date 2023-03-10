8380c4a78f04bcce946c6064a7b3f178512b3993
lihaan <45350323+lihaan@users.noreply.github.com>
2023-03-11 06:36:47 +0800

    change cors whitelist

--- smolify/src/main.ts
+++ smolify/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000"
+    origin: 'http://35.206.254.212',
   });
   await app.listen(8000); // 3000 is used by react
 }
